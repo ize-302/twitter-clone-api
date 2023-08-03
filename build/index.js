582baec7197ef21cedffc057c51723db563400b3
ize-302 <adavizeozorku@gmail.com>
2023-08-03 13:42:30 +0100

    remove build

--- build/index.js
+++ build/index.js
@@ -1,2 +1,22 @@
 "use strict";
-console.log("Hello world!");
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var express_1 = __importDefault(require("express"));
+var dotenv_1 = __importDefault(require("dotenv"));
+var body_parser_1 = __importDefault(require("body-parser"));
+var cors_1 = __importDefault(require("cors"));
+var cookie_parser_1 = __importDefault(require("cookie-parser"));
+dotenv_1.default.config();
+var index_1 = __importDefault(require("./routes/index"));
+var app = (0, express_1.default)();
+var port = process.env.PORT;
+app.use((0, cors_1.default)());
+app.use((0, cookie_parser_1.default)());
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: false }));
+app.use("/api/v1/", index_1.default);
+app.listen(port, function () {
+    console.log("App listening on port ".concat(port));
+});
